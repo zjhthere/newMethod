9ff6d19f7cd961c56c7c75e25179077382432d4a
zjhthere <751726931@qq.com>
2018-02-11 14:25:15 +0800

    new class

---
+++ CommonMethod/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  CommonMethod
+//
+//  Created by zhaojiahang on 2018/2/11.
+//  Copyright © 2018年 zhaojiahang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
