06ba4f5295aa619019622be2dd22de1a52072431
zjhthere <751726931@qq.com>
2018-02-11 14:07:15 +0800

    新加一个类

---
+++ CommonMethod/NewClass.h
@@ -0,0 +1,13 @@
+//
+//  NewClass.h
+//  CommonMethod
+//
+//  Created by zhaojiahang on 2018/2/11.
+//  Copyright © 2018年 zhaojiahang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewClass : NSObject
+
+@end
---
+++ CommonMethod/NewClass.m
@@ -0,0 +1,13 @@
+//
+//  NewClass.m
+//  CommonMethod
+//
+//  Created by zhaojiahang on 2018/2/11.
+//  Copyright © 2018年 zhaojiahang. All rights reserved.
+//
+
+#import "NewClass.h"
+
+@implementation NewClass
+
+@end
